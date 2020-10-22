b01458bff3bd6259b78ecd9c636674b18175fe0a
Loic Vourch <code@verybadfrags.com>
2020-10-21 21:39:27 -0700

    Clarify rules

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 const werRoles = [{ name: "🐺 Werewolf", team: "werewolves", verb: "Identify", description: "Action: Identify the role of a Player", id: "wolf" },
-{ name: "😈 Cultist", team: "werewolves", verb: "Threaten", description: "Action: Threaten someone during the night<br>Win: Same as Werewolves", id: "cultist" }];
+{ name: "😈 Cultist", team: "werewolves", verb: "Threaten", description: "Action: Threaten someone during the night", id: "cultist" }];
 const vilRoles = [
     { name: "🕵️ Detective", team: "villagers", verb: "Identify", description: "Action: Identify the role of a Player", id: "detective" },
     { name: "🧑‍🌾 Farmer", team: "villagers", verb: "Give corn to", description: "Action: Give corn to another player anonymously", id: "farmer" },
@@ -8,7 +8,7 @@ const vilRoles = [
     { name: "🧙 Fortune Teller", team: "villagers", verb: "Divine", description: "Action: Guess the team of a Player", id: "teller" },
     /*{ name: "👮 Jailer", team: "villagers", verb: "Block", description: "Action: Block a player from using his action during the night", id: "jailer" },*/
     { name: "🤴 Mayor", team: "villagers", verb: "Impress", description: "Action: Impress a Player - They will know your identity", id: "mayor" },
-    { name: "🤡 Jester", team: "special", verb: "Visit", description: "Action: Visit someone<br>Win: Get yourself exiled (You lose otherwise)", id: "jester" }];
+    { name: "🤡 Jester", team: "special", verb: "Visit", description: "Action: Visit someone (and do nothing)<br>Win: Get yourself exiled (You lose otherwise)", id: "jester" }];
 
 const avatars = [
     "🐱 Cat",
