1f92b14f6e712b42df0c33dc95f1eb2576daf6d4
Loic Vourch <code@verybadfrags.com>
2020-10-29 19:04:00 -0700

    Simplify python bundler script

--- bundle.py
+++ bundle.py
@@ -1,11 +1,11 @@
 # Inputs
 source_folder = "src/"
-input_html = source_folder + "index.html"
+input_html_path = source_folder + "index.html"
 build_folder = "build/"
-input_css = build_folder + "style.css"
-constants_js = build_folder + "constants.js"
-footer_js = build_folder + "footer.js"
 rules_path = build_folder + "rules.html"
+input_css_path = build_folder + "style.css"
+constants_js_path = build_folder + "constants.js"
+footer_js_path = build_folder + "footer.js"
 
 output_path = build_folder + "index.html"
 
@@ -19,7 +19,7 @@ def inject_file_content(input_path, replacement_tag, destination_content, prefix
 
 
 # Get HTML
-html_file = open(input_html, "r")
+html_file = open(input_html_path, "r")
 output_html = html_file.read()
 html_file.close()
 
@@ -29,29 +29,20 @@ output_html = inject_file_content(
 
 # Inject CSS
 output_html = inject_file_content(
-    input_css, '<link rel="stylesheet" type="text/css" href="style.css">', output_html, "<style>", "</style>")
+    input_css_path, '<link rel="stylesheet" type="text/css" href="style.css">', output_html, "<style>", "</style>")
 
 # Inject JS
 script_open = "<script>"
 script_close = "</script>"
 
-# Constants
-constants_file = open(constants_js, "r")
-js_link = '<script src="constants.js"></script>'
-output_html = output_html.replace(
-    js_link, script_open + constants_file.read() + script_close)
-constants_file.close()
-
-# Footer script
-footer_file = open(footer_js, "r")
-js_link = '<script src="footer.js"></script>'
-output_html = output_html.replace(
-    js_link, script_open + footer_file.read() + script_close)
-footer_file.close()
+output_html = inject_file_content(
+    constants_js_path, '<script src="constants.js"></script>', output_html, script_open, script_close)
+output_html = inject_file_content(
+    footer_js_path, '<script src="footer.js"></script>', output_html, script_open, script_close)
 
 # Output content
 output_file = open(output_path, "w")
 output_file.write(output_html)
 output_file.close()
 
-print("Bundled " + input_html + " into " + output_path)
+print("Bundled " + input_html_path + " into " + output_path)
